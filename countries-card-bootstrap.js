2c162c7ff782a74e0840d4c5297406ee14f1fd3f
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-02-05 13:10:15 +0530

    few more changes with bootsrap

--- countries-card-bootstrap.js
+++ countries-card-bootstrap.js
@@ -5,6 +5,12 @@ request.open('GET', 'https://restcountries.com/v3.1/all');
 
 request.send(null);
 
+const rootDiv = document.getElementById('root');
+rootDiv.style.display = 'flex';
+rootDiv.style.flexWrap = 'wrap';
+
+
+
 request.onload = function () {
   var response = JSON.parse(request.responseText);
 
@@ -14,13 +20,67 @@ request.onload = function () {
     console.log(flags, countryName);
     // create img card element from bootstrap
     // <div class="card" style="width: 18rem;">
-    //   <img src="..." class="card-img-top" alt="...">
+    //   <img src="{ svg | png }" class="card-img-top" alt="{ country alt | name }">
     //   <div class="card-body">
-    //     <h5 class="card-title">Card title</h5>
-    //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //     <h5 class="card-title">{ country name }</h5>
+    //     <p class="card-text">{ country alt }</p>
     //     <a href="#" class="btn btn-primary">Go somewhere</a>
     //   </div>
-    // </div>    
+    // </div>
+
+    const divElement = document.createElement('div');
+
+
+    // ** Do Not Use this way **
+    //     divElement.innerHTML = `<div class="card" style="width: 18rem;">
+    //   <img src="${flags.png}" class="card-img-top" alt="${flags.alt || countryName}">
+    //   <div class="card-body">
+    //     <h5 class="card-title">${countryName}</h5>
+    //     <p class="card-text">${flags.alt}</p>
+    //     <a href="#" class="btn btn-primary">Go somewhere</a>
+    //   </div>
+    // </div>`;
+
+    // creating individual element and structure using the methods of document
+    // inner to outer
+    const h5Element = document.createElement('h5');
+    h5Element.innerText = countryName;
+    h5Element.setAttribute('class', 'card-title');
+
+    const pElement = document.createElement('p');
+    pElement.innerText = flags.alt || countryName;
+    pElement.setAttribute('class', 'card-text');
+
+    const anchorElement = document.createElement('a');
+    anchorElement.innerText = 'Go somewhere';
+    anchorElement.setAttribute('href', '#');
+    anchorElement.setAttribute('class', 'btn btn-primary');
+
+    const cardBody = document.createElement('div');
+    cardBody.setAttribute('class', 'card-body');
+    cardBody.append(
+      h5Element,
+      pElement,
+      anchorElement
+    );
+
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', flags.svg);
+    imgElement.setAttribute('alt', flags.alt || countryName);
+    imgElement.setAttribute('class', 'card-img-top');
+
+    const card = document.createElement('div');
+
+    card.setAttribute('class', 'card');
+    card.style.width = '18rem';
+
+    card.append(
+      imgElement,
+      cardBody
+    );
+
+
+    rootDiv.appendChild(card);
   });
   console.log(response)
 }
